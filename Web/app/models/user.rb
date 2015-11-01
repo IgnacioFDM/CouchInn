c6c4bad949f2d502f0d95cf64dc8cd7c9c83537d
IgnacioDM <ignaciofdm@gmail.com>
2015-10-31 21:37:53 -0300

    Some remaining translations

--- Web/app/models/user.rb
+++ Web/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  enum role: [:user, :vip, :admin]
+  enum role: [:regular, :premium, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
