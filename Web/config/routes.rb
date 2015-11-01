7a5f256b0a59d7bde40ae2252f6e5b39d126066d
IgnacioDM <ignaciofdm@gmail.com>
2015-11-01 18:45:14 -0300

    Add "viewcouch" template

--- Web/config/routes.rb
+++ Web/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount Upmin::Engine => '/admin'
   root to: 'visitors#index'
+  match '/viewcouch' => 'visitors#viewcouch', :via => :get
   devise_for :users
   resources :users
 end
