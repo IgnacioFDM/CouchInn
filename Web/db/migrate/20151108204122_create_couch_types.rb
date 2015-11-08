ebb38f8b3ba85de6f2258546bd19063bee124c6f
IgnacioDM <ignaciofdm@gmail.com>
2015-11-08 20:56:05 -0300

    Fix couch types table missing created_at/updated_at

--- Web/db/migrate/20151108204122_create_couch_types.rb
+++ Web/db/migrate/20151108204122_create_couch_types.rb
@@ -2,6 +2,8 @@ class CreateCouchTypes < ActiveRecord::Migration
   def change
     create_table :couch_types do |t|
       t.string :name
+      
+      t.timestamps null: false
     end
   end
 end
