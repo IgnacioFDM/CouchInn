691e7fc7670b3821fc8493310d417554f668a588
IgnacioDM <ignaciofdm@gmail.com>
2015-11-08 20:26:40 -0300

    Fix seeds

--- Web/app/services/create_admin_service.rb
+++ Web/app/services/create_admin_service.rb
@@ -1,6 +1,6 @@
 class CreateAdminService
   def call
-    user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
+    user = User.find_or_create_by!(email: Rails.application.secrets.admin_email, telephone: Rails.application.secrets.admin_telephone, name: Rails.application.secrets.admin_name, birthdate: Date.new(1970, 01, 01)) do |user|
         user.password = Rails.application.secrets.admin_password
         user.password_confirmation = Rails.application.secrets.admin_password
         user.admin!
--- Web/db/seeds.rb
+++ Web/db/seeds.rb
@@ -10,14 +10,7 @@
 # que faltan segun la validación del modelo
 #user = CreateAdminService.new.call
 
-# Propongo --provisoriamente-- crear el usuario manualmente parecida a la implementación de CreateAdminService.call
-user = User.find_or_create_by!(email: Rails.application.secrets.admin_email,
-								telephone:"542215550000",
-								name:"Admin",
-								birthdate: Date.new(1994, 11, 10)) do |user|
-user.password = Rails.application.secrets.admin_password
-user.password_confirmation = Rails.application.secrets.admin_password
-user.admin!
+user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 
 couch_type = CouchType.find_or_create_by!(name: 'Choza')
@@ -25,6 +18,3 @@ puts 'CREATED COUCH TYPE: ' << couch_type.name
 
 couch_type = CouchType.find_or_create_by!(name: 'Casa')
 puts 'CREATED COUCH TYPE: ' << couch_type.name
-
-puts 'CREATED REGULAR USER: ' << user.name
-end
