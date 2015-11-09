f63a0f0447e9cfe955f06c87addcd97c9153543d
Ramiro Romero Dapozo <ramiroromero94@gmail.com>
2015-11-09 00:45:03 -0300

    Added "add couch type" functionality.

--- Web/app/controllers/couch_types_controller.rb
+++ Web/app/controllers/couch_types_controller.rb
@@ -1,11 +1,25 @@
 class CouchTypesController < ApplicationController
 
+  def new
+      @couch_type = CouchType.new
+  end
+
   def index
   	@couch_types = CouchType.all
   end
 
   def show
-  	@couch_type = CouchType.find(@params[:id])
+  	@couch_type = CouchType.find(params[:id])
+  end
+
+  def create
+    permitted =params.require(:couch_type).permit(:name)
+    @couch_type = CouchType.create permitted
+    if @couch_type
+      redirect_to couch_types_path, :notice => "Tipo creado"
+    else
+      redirect_to couch_types_path, :notice => "No se pudo crear el tipo!"
+    end
   end
 
   def update
