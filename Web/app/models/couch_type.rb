3592064a51e7cc7a57ba2c85d11c55c36b7222d8
Santiago <santiagojlevy@hotmail.com>
2015-11-10 19:32:56 -0300

    Added has_many CouchPosts to CouchType

--- Web/app/models/couch_type.rb
+++ Web/app/models/couch_type.rb
@@ -1,6 +1,6 @@
 class CouchType < ActiveRecord::Base
   # esto para cuando santi cree el modelo Couch
-  # belongs_to :couch
+   has_many :couch_posts
   validates_presence_of :name
   validates_uniqueness_of :name
 end
