38870e170aa3558e337fe98b477c88cdf230c1b9
Santiago <santiagojlevy@hotmail.com>
2015-11-11 01:59:08 -0300

    added users, couch posts, and couch types to the seed. Fixed the links in _couch_post partial view, now properly pointing to respective couchpost show.

--- Web/db/seeds.rb
+++ Web/db/seeds.rb
@@ -23,6 +23,36 @@ user.birthdate = Date.new(1994,2,1)
 user.save!
 puts 'CREATED REGULAR USER: ' << user.name
 
+user = User.new
+user.name = "Santiago"
+user.email = 'santiago@couchinn.com'
+user.password = 'password'
+user.password_confirmation = 'password'
+user.telephone = '0123456789'
+user.birthdate = Date.new(1994,04,25)
+user.save!
+puts 'CREATED REGULAR USER: ' << user.name
+
+user = User.new
+user.name = "Doggo"
+user.email = 'altoke_perro@dogchow.com'
+user.password = 'passwoof'
+user.password_confirmation = 'passwoof'
+user.telephone = '101'
+user.birthdate = Date.new(2005,2,1)
+user.save!
+puts 'CREATED REGULAR USER -kind of-: ' << user.name
+
+user = User.new
+user.name = "Coach Finn"
+user.email = 'Coach_Finn@couchinn.com'
+user.password = 'password'
+user.password_confirmation = 'password'
+user.telephone = '0123456789'
+user.birthdate = Date.new(1980,2,1)
+user.save!
+puts 'CREATED REGULAR USER: ' << user.name
+
 couch_type = CouchType.find_or_create_by!(name: 'Choza')
 puts 'CREATED COUCH TYPE: ' << couch_type.name
 
@@ -35,6 +65,15 @@ puts 'CREATED COUCH TYPE: ' << couch_type.name
 couch_type = CouchType.find_or_create_by!(name: 'Monoambiente')
 puts 'CREATED COUCH TYPE: ' << couch_type.name
 
+couch_type = CouchType.find_or_create_by!(name: 'Departamento')
+puts 'CREATED COUCH TYPE: ' << couch_type.name
+
+couch_type = CouchType.find_or_create_by!(name: 'Trailer')
+puts 'CREATED COUCH TYPE: ' << couch_type.name
+
+couch_type = CouchType.find_or_create_by!(name: 'Exterior')
+puts 'CREATED COUCH TYPE: ' << couch_type.name
+
 couch_post = CouchPost.find_or_create_by!(title: 'Casa de playa', description: 'Casa a 2 cuadras de la costa', couch_type_id: 2, username: 'Coach Finn', rooms: 1, vacants: 2, zone:'La Plata')
 puts 'CREATED COUCH POST: ' << couch_post.title
 
@@ -44,3 +83,15 @@ puts 'CREATED COUCH POST: ' << couch_post.title
 couch_post = CouchPost.find_or_create_by!(title: 'Carpa en Microcentro', description: 'Best option for humble tourists, make your reservations now!', couch_type_id: 3, username: 'Lionel Hutz', rooms: 1, vacants: 4, zone:'La Plata')
 
 couch_post = CouchPost.find_or_create_by!(title: 'Aula de Informática', description: 'No cierra la puerta así que duermo ahí por las noches, están todos invitados.', couch_type_id: 4, username: 'Bubi el perro', rooms: 1, vacants: 10, zone:'La Plata')
+
+couch_post = CouchPost.find_or_create_by!(title: 'Distorsión Espacio-Tiempo', description: 'Singularidad infinitamente amplia -literalmente-. Se está un poco apretado pero siempre hay lugar para uno más.', couch_type_id: 9, username: 'Gravitón', rooms: 1, vacants: 99, zone:'Sagittariius A*')
+puts 'CREATED COUCH POST: ' << couch_post.title
+
+couch_post = CouchPost.find_or_create_by!(title: 'Garage vacío', description: 'Tuve que vender el auto así que cuento con lugar extra para los que busquen un lugar donde quedarse un tiempo. Eso sí, ojo con las manchas de aceite.', couch_type_id: 4, username: 'Cacho', rooms: 1, vacants: 4, zone:'La Plata')
+puts 'CREATED COUCH POST: ' << couch_post.title
+
+couch_post = CouchPost.find_or_create_by!(title: 'Casa rodante completamente equipada', description: 'Ofrezco alojamiento y pago a cambio de colaboración con proyecto personal de Química. Cuantas menos cosas preguntes, mejor nos vamos a llevar.', couch_type_id: 6, username: 'Heisenberg', rooms: 1, vacants: 1, zone:'Los Hornos')
+puts 'CREATED COUCH POST: ' << couch_post.title
+
+couch_post = CouchPost.find_or_create_by!(title: 'Depósito del súpel', description: 'Chino tenel lugal. Lugal pala pelsona. No chico, glande, cole aile flesco.', couch_type_id: 2, username: 'Xin Xu Lin', rooms: 1, vacants: 2, zone:'La Plata')
+puts 'CREATED COUCH POST: ' << couch_post.title
