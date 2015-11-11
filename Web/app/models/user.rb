c3032136733a741724544645c75d453151000957
IDM <ignaciofdm@gmail.com>
2015-11-11 13:43:29 +0000

    Add extra validations to user

--- Web/app/models/user.rb
+++ Web/app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   enum role: [:user, :premium, :admin]
   enum gender: [:male, :female, :other]
-     has_many :couch_posts
+  has_many :couch_posts
+
   def self.i18n_genders(hash = {})
     genders.keys.each { |key| hash[I18n.t("gender_enum.#{key}")] = key }
     hash
@@ -16,7 +17,15 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-         
+  :recoverable, :rememberable, :trackable, :validatable
+
   validates :telephone, :name, :email, :birthdate, presence: true
+  validates :telephone, numericality: { only_integer:true }
+  validate :old_enough
+
+  def old_enough
+    if self.birthdate > 18.years.ago
+      errors.add(:birthdate, 'Debes ser mayor de 18 años')
+    end
+  end
 end
