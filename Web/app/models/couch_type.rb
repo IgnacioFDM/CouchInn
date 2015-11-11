a4840d67b306d4bb122bb2c623620d84e01e9f35
Ramiro Romero Dapozo <ramiroromero94@gmail.com>
2015-11-10 23:01:46 -0300

    Established dependency between CouchType and CouchPost.

--- Web/app/models/couch_type.rb
+++ Web/app/models/couch_type.rb
@@ -1,6 +1,6 @@
 class CouchType < ActiveRecord::Base
   # esto para cuando santi cree el modelo Couch
-   has_many :couch_posts
-  validates_presence_of :name
+   has_many :couch_posts , dependent: :restrict_with_exception
+  validates_presence_of :name 
   validates_uniqueness_of :name
 end
