7b9d3ce490efc40f3ea4d9fa669b857fe320173d
Ramiro Romero Dapozo <ramiroromero94@gmail.com>
2015-11-13 22:33:58 -0300

    Made CouchType's name non-case-sensitive

--- Web/app/models/couch_type.rb
+++ Web/app/models/couch_type.rb
@@ -1,6 +1,15 @@
 class CouchType < ActiveRecord::Base
-  # esto para cuando santi cree el modelo Couch
-   has_many :couch_posts , dependent: :restrict_with_exception
+  before_validation :downcase_name
+  has_many :couch_posts , dependent: :restrict_with_exception
   validates_presence_of :name 
   validates_uniqueness_of :name
+
+  def is_disabled?
+  	not self.disabled_at.blank?
+  end
+
+  private
+  def downcase_name
+  	self.name = self.name.downcase
+  end
 end
