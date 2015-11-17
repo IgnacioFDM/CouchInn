268795899cf96c38909cce550651b4d5940ee799
Ramiro Romero Dapozo <ramiroromero94@gmail.com>
2015-11-17 20:43:39 -0300

    Added and implemented 'new' form for CouchReservationRequest.

--- Web/app/controllers/couch_reservation_requests_controller.rb
+++ Web/app/controllers/couch_reservation_requests_controller.rb
@@ -1,22 +1,45 @@
 class CouchReservationRequestsController < ApplicationController
+  before_action :authenticate_user!
+  after_action :verify_authorized
+
   def new
+    authorize CouchReservationRequest
+    @couch_reservation_request = CouchReservationRequest.new
+    @couch_reservation_request.couch_post_id = params.require(:couch_post_id).to_i
   end
 
   def create
-  end
+    authorize CouchReservationRequest
+    parameters = params.require(:couch_reservation_request).permit(:start_date,:end_date,:amount,:couch_post_id)
+    @couch_reservation_request = CouchReservationRequest.new 
+    @couch_reservation_request.user_id = current_user.id
+    @couch_reservation_request.couch_post_id = parameters[:couch_post_id]
+    @couch_reservation_request.start_date = parameters[:start_date]
+    @couch_reservation_request.end_date = parameters[:end_date]
+    @couch_reservation_request.amount = parameters[:amount]
+    success = @couch_reservation_request.save
+    if success
+      redirect_to couch_reservation_requests_path, notice: "Pedido hecho." 
+    else
+      redirect_to couch_reservation_requests_path, alert: "No se pudo hacer pedido: " << @couch_reservation_request.errors.full_messages.to_sentence
+    end
 
+  end
+    
   def update
-  end
-
-  def edit
+    authorize CouchReservationRequest
   end
 
   def destroy
+    authorize CouchReservationRequest
   end
 
   def index
+    authorize CouchReservationRequest
   end
 
   def show
+    authorize CouchReservationRequest
   end
+
 end
