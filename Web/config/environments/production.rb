3841d61adc0e8c4b1ae577c7475401270262a995
IDM <ignaciofdm@gmail.com>
2015-11-19 19:53:58 +0000

    Fix email 3

--- Web/config/environments/production.rb
+++ Web/config/environments/production.rb
@@ -77,8 +77,12 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-reply@couchinn.ignaciodm.com'}
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address: 'localhost',
+    port: 25
+  }
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  config.action_mailer.default_url_options = {host: Rails.application.secrets.domain_name}
 end
