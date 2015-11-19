4f110b204223e72deaa9cf765590f5998bd11f07
IDM <ignaciofdm@gmail.com>
2015-11-19 20:04:00 +0000

    Allow visitors to browse couches

--- Web/app/controllers/couch_posts_controller.rb
+++ Web/app/controllers/couch_posts_controller.rb
@@ -1,5 +1,5 @@
 class CouchPostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :except => [:index, :show]
   after_action :verify_authorized
 
   def new
