ef3c04bbebb71e215aa1489a19cf0aec8baf6a86
Ramiro Romero Dapozo <ramiroromero94@gmail.com>
2015-11-30 18:40:23 -0300

    Fix UserFeedback model.

--- Web/db/schema.rb
+++ Web/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120150446) do
+ActiveRecord::Schema.define(version: 20151130212215) do
 
   create_table "average_caches", force: :cascade do |t|
     t.integer  "rater_id"
@@ -105,6 +105,7 @@ ActiveRecord::Schema.define(version: 20151120150446) do
     t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "author_id"
   end
 
   create_table "users", force: :cascade do |t|
